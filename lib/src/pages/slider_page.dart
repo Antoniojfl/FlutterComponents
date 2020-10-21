ce8b1c1a116d9737c747fc9c461a748330ca9bf1
Antonio Fernandez <antonioptc23@gmail.com>
2020-10-21 00:01:26 -0300

    checkbox and switchBox

--- lib/src/pages/slider_page.dart
+++ lib/src/pages/slider_page.dart
@@ -6,6 +6,7 @@ class SliderPage extends StatefulWidget {
 
 class _SliderPageState extends State<SliderPage> {
   double _valueSlider = 0.0;
+  bool _sliderStatus = true;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,6 +20,8 @@ class _SliderPageState extends State<SliderPage> {
               padding: EdgeInsets.only(top: 30),
             ),
             _createSlider(),
+            _createCheckBox(),
+            _createSwichBox(), // Se activan y desactivan ambos porque el estado controla la misma propiedad
             _addImage(),
           ],
         ),
@@ -29,18 +32,19 @@ class _SliderPageState extends State<SliderPage> {
   Widget _createSlider() {
     return Slider(
       activeColor: Colors.indigoAccent,
-      label: 'Tamaño de la Imagen',
+      label: "Valor: $_valueSlider", // Sin el division No muestra esto
       value: _valueSlider,
       max: 400.0,
       min: 0.0,
       divisions: 200,
-      onChanged: (double value) {
-        //print(value);
-        setState(() {
-          _valueSlider = value;
-        });
-      },
+      onChanged: _sliderStatus ? _activateDeactivateSlider : null,
     );
+  }
+
+  void _activateDeactivateSlider(double value) {
+    setState(() {
+      _valueSlider = value;
+    });
   }
 
   Widget _addImage() {
@@ -50,6 +54,47 @@ class _SliderPageState extends State<SliderPage> {
             "https://assets.stickpng.com/images/5ede4a3fb760540004f2c5e9.png"),
         width: _valueSlider,
       ),
+    );
+  }
+
+  Widget _createCheckBox() {
+    //return Row(
+    //  children: [
+    //    Text('Activar/Desactivar Slider: '),
+    //    Checkbox(
+    //      value: _sliderStatus,
+    //      onChanged: (value) {
+    //        setState(() {
+    //          _sliderStatus = value;
+    //          print(value);
+    //        });
+    //      },
+    //    )
+    //  ],
+    //);
+
+    return CheckboxListTile(
+      title: (Text('Activar/Desactivar Slider: ')),
+      value: _sliderStatus,
+      onChanged: (value) {
+        setState(() {
+          _sliderStatus = value;
+          print(value);
+        });
+      },
+    );
+  }
+
+  Widget _createSwichBox() {
+    return SwitchListTile(
+      title: (Text('Activar/Desactivar Slider: ')),
+      value: _sliderStatus,
+      onChanged: (value) {
+        setState(() {
+          _sliderStatus = value;
+          print(value);
+        });
+      },
     );
   }
 }
